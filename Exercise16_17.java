0bc5a01c038cb8b5e353097e575984fdb413dc4a
Kira EB <107140669+KemmettBuck@users.noreply.github.com>
2026-07-13 15:00:50 -0400

    add javaFX wildcard imports, extend Application in class

--- Exercise16_17.java
+++ Exercise16_17.java
@@ -1,4 +1,22 @@
-public class Exercise16_17 {
+/**
+ * Exercise16_17
+ * Display "Show Colors" with sliders to
+ * control colors: red, green, blue, and the opacity
+ * 
+ * Author: Kira Emmett Buck
+ * Course: Java - Ivy Tech Community College
+ */
+
+import javafx.application.*;
+import javafx.geometry.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.*;
+import javafx.scene.text.*;
+import javafx.stage.*;
+
+public class Exercise16_17 extends Application {
     
     public static void main(String[] args) {
         launch(args);
